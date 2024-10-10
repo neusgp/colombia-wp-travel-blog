a72dd84dd63df4279f7c0da2d4b84d67551201fb
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-10-10 18:31:20 +0200

    refactor: tidy up functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,20 @@
 <?php
 //TODO: rename to colombiablog when migrating to host.
+//TODO: refactor this
+
+//NOTES
+//css -> parameter 1: nickname for files, parameter 2: location of the file.
+//get_stylesheet_uri() -> gets css from root folder.
+
 function learning_wp_files()
 {
     wp_register_style('quicksand', 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap');
     wp_register_style('playwrite', 'https://fonts.googleapis.com/css2?family=Playwrite+CO:wght@100..400&display=swap');
-    //css -> parameter 1: nickname for files, parameter 2: location of the file.
-    //get_stylesheet_uri() -> gets css from root folder.
-    wp_enqueue_style('learning_wp_main_styles', get_stylesheet_uri());
-    wp_enqueue_style('learning_wp_main_styles', get_stylesheet_uri());
-    //TODO: refactor this
-    wp_enqueue_style('custom-google-stlye', '//fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
     wp_enqueue_style('quicksand');
     wp_enqueue_style('playwrite');
+
+    wp_enqueue_style('learning_wp_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('learning_wp_main_styles', get_stylesheet_uri());
 }
 
 //second param is our function. We are no executing the function right away, wp handles that for us. 
