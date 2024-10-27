7dd4188be20fea639afa4fdb38ca454c567be448
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-10-27 17:31:48 +0100

    fix: load js correctly

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@
 
 function learning_wp_files()
 {
+    //load js from build folder
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    //load css from build folder
     wp_register_style('spectral', 'https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
     wp_register_style('courier prime', 'https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
     wp_enqueue_style('spectral');
